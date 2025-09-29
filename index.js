99e79fae742d688fdea45cd61895734fa5f15063
Max <max.eisenstein@jbha.org>
2025-09-29 01:17:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { HashRouter } from "react-router-dom";
+import App from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/test-site">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
